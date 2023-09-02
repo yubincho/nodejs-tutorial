f90013af26a75abf62ce21bbb14273c5bfcf93c9
yubin <yubincho9@gmail.com>
2023-09-02 15:29:48 +0900

    add Validation

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,6 +1,21 @@
 import {Request, Response} from "express";
+import {RegisterValidation} from "../validation/register.validation";
 
 
 export const Register = (req: Request, res: Response) => {
-    res.send(req.body)
+    const body = req.body
+
+    const { error } = RegisterValidation.validate(body)
+
+    if (error) {
+        return res.status(400).send(error.details)
+    }
+
+    if (body.password !== body.password_confirm) {
+        return res.status(400).send({
+            message: "Passwords do not match"
+        })
+    }
+
+    res.send(body)
 }
