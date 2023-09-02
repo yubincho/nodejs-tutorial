1b36ae990d49a7b2d3115eb836ee9d26fac5a148
yubin <yubincho9@gmail.com>
2023-09-02 15:34:12 +0900

    add Validation-email 수정

--- src/validation/register.validation.ts
+++ src/validation/register.validation.ts
@@ -3,7 +3,7 @@ import {Joi} from "express-validation";
 export const RegisterValidation = Joi.object({
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
     password_confirm: Joi.string().required(),
 })
