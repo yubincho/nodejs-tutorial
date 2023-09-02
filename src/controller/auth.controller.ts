53f7fb1a5cd4f68ad08ff8a579f2ff472da16f63
yubin <yubincho9@gmail.com>
2023-09-02 15:08:59 +0900

    add Routes

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,6 @@
+import {Request, Response} from "express";
+
+
+export const Register = (req: Request, res: Response) => {
+    res.send(req.body)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors'
+import {routes} from "./routes";
 
 const app = express()
 
@@ -11,6 +12,9 @@ app.use(cors({
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World')
 })
+
+routes(app)
+
 
 app.listen(8000, () => {
     console.log('listening to port 8000')
