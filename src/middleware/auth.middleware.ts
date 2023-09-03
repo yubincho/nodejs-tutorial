fecf45066017abc65d678db41017ac6ffd49af6b
yubin <yubincho9@gmail.com>
2023-09-03 22:19:01 +0900

    middleware_authenticate & logout 로직 추가 1

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,30 @@
+import {Request, Response} from "express";
+import AppDataSource from "../database/ormConfig";
+import {User} from "../entities/user.entity";
+import { verify } from 'jsonwebtoken'
+
+
+export const AuthMiddleware = async (req: Request, res: Response, next: Function) => {
+    try{
+        const jwt = req.cookies['jwt']
+
+        const payload: any = verify(jwt, process.env.SECRET_KEY)
+
+        if (!payload) {
+            return res.status(401).send({
+                message: 'unauthenticated'
+            })
+        }
+
+        const repository = AppDataSource.getRepository(User)
+
+        const { password, ...user} = await repository.findOneBy(payload.id)
+
+        // res.se
+        next()
+    } catch (e) {
+        return res.status(401).send({
+            message: 'unauthenticated'
+        })
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import {AuthenticatedUser, Login, Logout, Register} from "./controller/auth.controller";
+import {AuthMiddleware} from "./middleware/auth.middleware";
 
 
 export const routes = (router: Router) => {
     router.post('/api/register', Register)
     router.post('/api/login', Login)
-    router.get('/api/user', AuthenticatedUser)
-    router.get('/api/logout', Logout)
+    router.get('/api/user', AuthMiddleware, AuthenticatedUser)
+    router.get('/api/logout', AuthMiddleware, Logout)
 }
